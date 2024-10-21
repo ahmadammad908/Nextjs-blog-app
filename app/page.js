3640065ac248d5aadb162cc59ef008b2d1ec3691
ahmadammad908 <ahmadammad789@gmail.com>
2024-10-21 17:21:53 +0500

    Your commit message

--- app/page.js
+++ app/page.js
@@ -1,101 +1,84 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main>
+      Sexual intercourse
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      Article
+      Talk
+      Read
+      View source
+      View history
+
+      Tools
+      Appearance hide
+      Text
+
+      Small
+
+      Standard
+
+      Large
+      Width
+
+      Standard
+
+      Wide
+      Color (beta)
+
+      Automatic
+
+      Light
+
+      Dark
+      Page semi-protected
+      From Wikipedia, the free encyclopedia
+      This article is primarily about humans. For other animals, see Animal sexual behaviour. For other uses, see Sex (disambiguation).
+      "Copulation" redirects here. For copulation among other animals, see Copulation (zoology). For the biological functions of sexual intercourse, see Penile-vaginal intercourse.
+
+      Sexual intercourse in the missionary position depicted by Édouard-Henri Avril (1892)
+      Sexual intercourse (also coitus or copulation) is a sexual activity typically involving the insertion and thrusting of the male penis inside the female vagina for sexual pleasure, reproduction, or both.[1] This is also known as vaginal intercourse or vaginal sex.[2][3] Sexual penetration has been known by humans since the dawn of time, and has been an instinctive form of sexual behaviour and psychology among humans. Other forms of penetrative sexual intercourse include anal sex (penetration of the anus by the penis), oral sex (penetration of the mouth by the penis or oral penetration of the female genitalia), fingering (sexual penetration by the fingers) and penetration by use of a dildo (especially a strap-on dildo), and vibrators.[4][5] These activities involve physical intimacy between two or more people and are usually used among humans solely for physical or emotional pleasure. They can contribute to human bonding.[4][6]
+
+      There are different views on what constitutes sexual intercourse or other sexual activity, which can impact on views of sexual health.[7] Although sexual intercourse, particularly the term coitus, generally denotes penile–vaginal penetration and the possibility of creating offspring,[1] it also commonly denotes penetrative oral sex and penile–anal sex, especially the latter.[8] It usually encompasses sexual penetration, while non-penetrative sex has been labeled outercourse,[9] but non-penetrative sex may also be considered sexual intercourse.[4][10] Sex, often a shorthand for sexual intercourse, can mean any form of sexual activity.[7] Because people can be at risk of contracting sexually transmitted infections during these activities, safer sex practices are recommended by health professionals to reduce transmission risk.[11][12]
+
+      Various jurisdictions place restrictions on certain sexual acts, such as adultery, incest, sexual activity with minors, prostitution, rape, zoophilia, sodomy, premarital sex and extramarital sex. Religious beliefs also play a role in personal decisions about sexual intercourse or other sexual activity, such as decisions about virginity,[13][14] or legal and public policy matters. Religious views on sexuality vary significantly between different religions and sects of the same religion, though there are common themes, such as prohibition of adultery.
+
+      Reproductive sexual intercourse between non-human animals is more often called copulation, and sperm may be introduced into the female's reproductive tract in non-vaginal ways among the animals, such as by cloacal copulation. For most non-human mammals, mating and copulation occur at the point of estrus (the most fertile period of time in the female's reproductive cycle), which increases the chances of successful impregnation.[15][16] However, bonobos, dolphins and chimpanzees are known to engage in sexual intercourse regardless of whether the female is in estrus, and to engage in sex acts with same-sex partners.[17] Like humans engaging in sexual activity primarily for pleasure, this behavior in these animals is also presumed to be for pleasure, and a contributing factor to strengthening their social bonds.[18]
+
+      Behaviors
+      See also: Human sexual activity and Human sexuality
+      Definitions
+
+      Modern artwork of a couple engaged in vaginal intercourse
+
+      19th-century erotic interpretation of Roman emperor Hadrian and Antinous engaged in anal intercourse, by Édouard-Henri Avril
+      Sexual intercourse may be called coitus, copulation, coition, or intercourse. Coitus is derived from the Latin word coitio or coire, meaning "a coming together or joining together" or "to go together", and is known under different ancient Latin names for a variety of sexual activities, but usually denotes penile–vaginal penetration.[19] This is often called vaginal intercourse or vaginal sex.[2][20] Vaginal sex, and less often vaginal intercourse, may also denote any vaginal sexual activity, particularly if penetrative, including sexual activity between lesbian couples.[21][22] Copulation, by contrast, more often denotes the mating process, especially for non-human animals; it can mean a variety of sexual activities between opposite-sex or same-sex pairings,[23] but generally means the sexually reproductive act of transferring sperm from a male to a female or sexual procreation between a man and a woman.[23][24][25]
+
+      Although sex and having sex also most commonly denote penile–vaginal intercourse,[26] sex can be significantly broad in its meaning and may cover any penetrative or non-penetrative sexual activity between two or more people.[7] The World Health Organization (WHO) states that non-English languages and cultures use different words for sexual activity, "with slightly different meanings".[7] Various vulgarisms, slang, and euphemisms are used for sexual intercourse or other sexual activity, such as fuck, screw, shag, and the phrase "sleep together".[27][28][29] The laws of some countries use the euphemism, carnal knowledge. Penetration of the vagina by the erect penis is additionally known as intromission, or by the Latin name immissio penis (Latin for "insertion of the penis").[30] The age of first sexual intercourse is called sexarche.[31][32]
+
+      Vaginal, anal and oral sex are recognized as sexual intercourse more often than other sexual behaviors.[33] Sexual activity that does not involve penile-vaginal sex or other sexual penetration might be used to retain virginity (sometimes called technical virginity) or labeled outercourse.[34] One reason virginity loss is often based on penile–vaginal intercourse is because heterosexual couples may engage in anal or oral sex as a way of being sexually active while maintaining that they are virgins since they have not engaged in the reproductive act of coitus.[35] Some gay men consider frot or oral sex as a way of maintaining their virginities, with penile-anal penetration used as sexual intercourse and for virginity loss, while other gay men may consider frot or oral sex as their main forms of sexual activity.[13][36][37] Lesbians may categorize oral sex or fingering as sexual intercourse and subsequently an act of virginity loss,[13][38] or tribadism as a primary form of sexual activity.[39][40]
+
+      Researchers commonly use sexual intercourse to denote penile–vaginal intercourse while using specific words, such as anal sex or oral sex, for other sexual behaviors.[41] Scholars Richard M. Lerner and Laurence Steinberg state that researchers also "rarely disclose" how they conceptualize sex "or even whether they resolved potential discrepancies" in conceptualizations of sex.[38] Lerner and Steinberg attribute researchers' focus on penile–vaginal sex to "the larger culture's preoccupation with this form of sexual activity", and have expressed concern that the "widespread, unquestioned equation of penile–vaginal intercourse with sex reflects a failure to examine systematically 'whether the respondent's understanding of the question [about sexual activity] matches what the researcher had in mind'".[38] This focus can also relegate other forms of mutual sexual activity to foreplay or contribute to them not being regarded as "real sex", and limits the meaning of rape.[42][43] It may also be that conceptually conflating sexual activity with vaginal intercourse and sexual function hinders and limits information about sexual behavior that non-heterosexual people may be engaging in, or information about heterosexuals who may be engaging in non–vaginal sexual activity.[42]
+
+      Studies regarding the meaning of sexual intercourse sometimes come into conflict. While most consider penile–vaginal intercourse to be sex, whether anal or oral intercourse are considered sex is more debatable, with oral sex ranking lowest.[44][45] The Centers for Disease Control and Prevention (CDC) stated that "although there are only limited national data about how often adolescents engage in oral sex, some data suggest that many adolescents who engage in oral sex do not consider it to be 'sex'; therefore they may use oral sex as an option to experience sex while still, in their minds, remaining abstinent".[46] Upton et al. stated, "It is possible that individuals who engage in oral sex, but do not consider it as 'sex', may not associate the acts with the potential health risks they can bring."[44] In other cases, condom use is a factor, with some men stating that sexual activity involving the protection of a condom is not "real sex" or "the real thing".[47][48] This view is common among men in Africa,[47][48] where sexual activity involving the protection of a condom is often associated with emasculation because condoms prevent direct penile–to–skin genital contact.[47]
+
+      Stimulation
+      Sexual intercourse or other sexual activity can encompass various sexually stimulating factors (physiological stimulation or psychological stimulation), including different sex positions (such as the missionary position, the most common human sex position[49]) or the use of sex toys.[50][51] Foreplay may precede some sexual activities, often leading to sexual arousal of the partners and resulting in the erection of the penis or natural lubrication of the vagina.[52] It is also common for people to be as sexually satisfied by being kissed, touched erotically, or held as they are during sexual intercourse.[53]
+
+      Non-primate females copulate only when in estrus,[54] but sexual intercourse is possible at any time of the menstrual cycle for women.[55][56] Sex pheromones facilitate copulatory reflexes in various organisms, but, in humans, the detection of pheromones is impaired and they have only residual effects.[57] Non-primate females put themselves in the crucial lordosis position and remain motionless, but these motor copulatory reflexes are no longer functional in women.[54]
+
+
+      Édouard-Henri Avril depiction of a woman on top position, a position that is more likely to stimulate the clitoris[58]
+      During coitus, the partners orient their hips to allow the penis to move back and forth in the vagina to cause friction, typically without fully removing the penis. In this way, they stimulate themselves and each other, often continuing until orgasm in either or both partners is achieved.[10][59]
+
+      For human females, stimulation of the clitoris plays a significant role in sexual activity; 70–80% of women require direct clitoral stimulation to achieve orgasm,[60][61][62] though indirect clitoral stimulation (for example, via vaginal intercourse) may also be sufficient (see orgasm in females).[63][64] Because of this, some couples may engage in the woman on top position or the coital alignment technique, a technique combining the "riding high" variation of the missionary position with pressure-counterpressure movements performed by each partner in rhythm with sexual penetration, to maximize clitoral stimulation.[58][65]
+
+
+      Édouard-Henri Avril depiction of cunnilingus in the life of Sappho
+      Anal sex involves stimulation of the anus, anal cavity, sphincter valve or rectum; it most commonly
+    </main>
+  )
 }
+
+export default page
---
+++ components/Navbar.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import Link from 'next/link';
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { ModeToggle } from '../components/theme-btn';
+const Navbar = () => {
+  return (
+    <nav className="p-4 bg-background/50 sticky top-0 backdrop-blur border-b">
+      <div className="container mx-auto flex justify-between   items-center"> 
+        <div className="text-xl font-bold">
+          <Link href="/">
+            Ahmad Blog
+          </Link>
+        </div>
+        <ul className="md:flex hidden space-x-4 items-center">
+          <Link href="/" className="hover:scale-105 hover:font-semibold transition-transform duration-300 ">
+            Home
+          </Link>
+          <Link href="/"  className="hover:scale-105 hover:font-semibold transition-transform duration-300 ">
+            Blog
+          </Link>
+          <Link href="/about"   className="hover:scale-105 hover:font-semibold transition-transform duration-300 ">
+            About Us
+          </Link>
+          <Link href="/contact"  className="hover:scale-105 hover:font-semibold transition-transform duration-300 ">
+            Contact
+          </Link>
+          <div className='flex items-center' >
+            <Button variant="outline" className="mx-1">Login</Button>
+            <Button variant="outline" className="mx-1">Sign Up</Button>
+            <ModeToggle/>
+
+          </div>
+        </ul>
+        <div className='md:hidden flex justify-center items-center'>
+        <span  className="mx-2">
+            <ModeToggle/>
+
+            </span>
+          <Sheet>
+            <SheetTrigger className='flex'>
+
+              <svg className='w-6 h-6' fill="none" stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+              </svg>
+
+            </SheetTrigger>
+          
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle className="text-bold my-4">Ahmad Blog</SheetTitle>
+                <SheetDescription>
+                  <ul className='flex flex-col gap-6' >
+                    <Link href="/" className="hover:text-gray-400 ">
+                      Home
+                    </Link>
+                    <Link href="/" className="hover:text-gray-400 ">
+                      Blog
+                    </Link>
+                    <Link href="/about" className="hover:text-gray-400 ">
+                      About Us
+                    </Link>
+                    <Link href="/contact" className="hover:text-gray-400">
+                      Contact
+                    </Link>
+                    <div>
+                      <Button variant="outline" className="mx-1 text-xs">Login</Button>
+                      <Button variant="outline" className="mx-1 text-xs">Sign Up</Button>
+                    </div>
+                  </ul>
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+
+
+
+    </nav>
+  );
+};
+
+export default Navbar;
